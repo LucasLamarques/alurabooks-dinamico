6464a09999489b41398accb56056cf962925e349
Lucas Lamarques <lucas25maia@gmail.com>
2023-11-18 00:54:20 -0300

    Modificação banner

--- app/metodoMap.js
+++ app/metodoMap.js
@@ -2,6 +2,6 @@ function aplicarDesconto(livros) {
     const desconto = 0.3
     livrosComDesconto = livros.map(livro => {
         return {...livro, preco: livro.preco - (livro.preco * desconto)} // faz uma copia do array modificando o preço
-    })
+    });
     return livrosComDesconto
 }
--- app/metodoSort.js
+++ app/metodoSort.js
@@ -2,6 +2,6 @@ let btnOdenarPorPreco = document.querySelector('#btnOrdenarPorPreco')
 btnOdenarPorPreco.addEventListener('click', ordenarLivrosPorpreco) ;
 
     function ordenarLivrosPorpreco() {
-        let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
-        exibirOsLivrosNaTela(livrosOrdenados)
+        let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco);
+        exibirOsLivrosNaTela(livrosOrdenados);
     }
